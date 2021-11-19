ea26489cba609c4a07dca3a6d4996e3ce61b9dab
Raman Sailopal <thelinuxguys@yahoo.com>
2021-11-19 17:01:48 +0000

    Comments weren't formatted correctly

--- data/r1.32_x86_64/r/movies.sql
+++ data/r1.32_x86_64/r/movies.sql
@@ -1,16 +1,18 @@
---Create Database
- CREATE database movies;
---Switch Database
- USE movies;
---Schema
- CREATE TABLE titles(name varchar(255), director varchar(255), year YEAR, country varchar(255), rating varchar(255));
---Insert
- INSERT INTO movies.titles (name, director, year, country, rating) VALUES ('hello world', 'Tomohiko Ito', 2019, 'Japan', 'average');
---Query
- SELECT * FROM movies.titles WHERE name = 'hello world';
---Update
- UPDATE movies.titles SET rating = 'good' WHERE name = 'hello world';
- --Delete
- DELETE FROM movies.titles WHERE name = 'hello world';
+/*Create Database*/
+CREATE database movies;
+/*Switch Database*/
+USE movies;
+/*Schema*/
+CREATE TABLE titles(name varchar(255), director varchar(255), year YEAR, country varchar(255), rating varchar(255));
+/*Insert*/
+INSERT INTO movies.titles (name, director, year, country, rating) VALUES ('hello world', 'Tomohiko Ito', 2019, 'Japan', 'average');
+/*Query*/
+SELECT * FROM movies.titles WHERE name = 'hello world';
+/*Update*/
+UPDATE movies.titles SET rating = 'good' WHERE name = 'hello world';
+/*Query*/
+SELECT * FROM movies.titles WHERE name = 'hello world';
+/*Delete*/
+DELETE FROM movies.titles WHERE name = 'hello world';
 
     
