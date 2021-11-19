9acabb474070e8c7a0bfbd373931cf5917c34e35
Raman Sailopal <thelinuxguys@yahoo.com>
2021-11-19 10:45:43 +0000

    Tidying queries

--- data/r1.32_x86_64/r/movies.sql
+++ data/r1.32_x86_64/r/movies.sql
@@ -1,9 +1,13 @@
+--Create Database
+ CREATE database movies;
+--Switch Database
+ USE movies;
+--Schema
+ CREATE TABLE titles(name varchar(255), director varchar(255), year YEAR, country varchar(255), rating varchar(255));
+--Insert
+ INSERT INTO movies.titles (name, director, year, country, rating) VALUES ('hello world', 'Tomohiko Ito', 2019, 'Japan', 'average');
 --Query
  SELECT * FROM movies.titles WHERE name = 'hello world';
---Schema
- CREATE TABLE movies(name varchar(255), director varchar(255), year YEAR, country varchar(255), rating varchar(255));
---Insert
- INSERT INTO movies.titles (name, director, rating, year) VALUES ('hello world', 'Tomohiko Ito', 2019, 'Japan', 'average');
 --Update
  UPDATE movies.titles SET rating = 'good' WHERE name = 'hello world';
  --Delete
