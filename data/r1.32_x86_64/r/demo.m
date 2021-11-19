a150ab0d62d1ff8a8b158c3c41e5d76f2c4c0a9c
Raman Sailopal <thelinuxguys@yahoo.com>
2021-11-19 10:04:58 +0000

    Update demo.m
    Demo now based around hello world movie

--- data/r1.32_x86_64/r/demo.m
+++ data/r1.32_x86_64/r/demo.m
@@ -1,2 +1,16 @@
- Write "Hello this is a demo of a routine written for YottaDB and run in Gitpod"
- Quit
+hello()
+	write "Hello, World!",!
+	quit
+Write()
+	set ^MOVIES("hello world")=""
+	set ^MOVIES("hello world","director")="Tomohiko Ito"
+	set ^MOVIES("hello world","year")="2019"
+	set ^MOVIES("hello world","country")="Japan"
+	set ^MOVIES("hello world","rating")="average"
+	quit
+Update()
+	set ^MOVIES("hello world","rating")="good"
+	quit
+Delete()
+	kill ^MOVIES("hello world")
+	quit
